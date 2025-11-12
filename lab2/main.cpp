e07fc782dc77f37daf309154c09e636a0b988c15
A <666av6@gmail.com>
2025-11-12 08:33:58 +0300

    upd

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,18 +1,20 @@
-#include"../main.hpp"
+#include "../main.hpp"
 
 /// Главная функция: запускает тестовые примеры и выводит деревья разбора.
-int main() {
+int main()
+{
     std::vector<std::string> tests = {
         "while (x < V) y := I done",
-        "while (a = I) b := X done; while (n > III) m := a done"
-    };
+        "while (a = I) b := X done; while (n > III) m := a done"};
 
-    for (size_t i = 0; i < tests.size(); ++i) {
+    for (size_t i = 0; i < tests.size(); ++i)
+    {
         std::cout << "=== Тест " << (i + 1) << " ===\n";
         std::cout << "Входная строка: " << tests[i] << "\n\n";
-        
+
         auto tokens = tokenize(tests[i]);
-        if (tokens.empty()) {
+        if (tokens.empty())
+        {
             std::cout << "Лексический анализ не удался.\n\n";
             continue;
         }
@@ -21,15 +23,20 @@ int main() {
         std::cout << "=== LR-анализ ===\n";
         LRParser parser(tokens);
         auto ast = parser.parse();
-        
-        if (ast) {
+
+        if (ast)
+        {
             std::cout << "\n=== Результат AST ===\n";
             printAST(ast);
-        } else {
+        }
+        else
+        {
             std::cout << "LR-анализ завершился с ошибками\n";
         }
-        
-        std::cout << "\n" << std::string(40, '=') << "\n\n";
+
+        std::cout << "\n"
+                  << std::string(40, '=') << "\n\n";
+
     }
 
     return 0;
